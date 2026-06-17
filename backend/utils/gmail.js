6028676af790dad0ebd8012ad8d6006dac51d4d2
Rahul-49 <sonurahulmadduri99@gmail.com>
2026-06-17 08:51:13 +0530

    Implemented Backend MVC arch and Login Register Routes

---
+++ backend/utils/gmail.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+const path = require('path');
+
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env')
+});
+
+const body = `
+<h2>User Registered Successfully</h2>
+<p>Your email has been registered successfully.</p>
+`;
+
+const mail = async (email, username) => {
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  const msg = {
+    from: `"Sonu" ${process.env.SMTP_USER}`,
+    to: email,
+    subject: `${username} Account Creation`,
+    text: "Hello World",
+    html: body,
+  };
+
+  try {
+    const info = await transporter.sendMail(msg);
+
+    console.log("Message sent:", info.messageId);
+  } catch (err) {
+    console.error("Error while sending mail:", err);
+  }
+};
+
+module.exports = mail;
